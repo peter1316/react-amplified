921da61cad72d9c4dbe0daa1e42f57fb323a5563
Peter Huang <peter.huang@belkin.com>
2023-09-19 19:32:40 +0800

    amplify add api & amplify push

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactamplified": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "reactamplifiedcd589bda": {
       "AppClientID": "string",
